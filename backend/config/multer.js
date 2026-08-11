71762c9c3b69447bb16fc83285766ff561bb5a70
csahmad <ahmadalmlkyy@gmail.com>
2026-08-11 14:22:42 +0300

    Update multer.js

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB حد أقصى لحجم الصورة
+    fileSize: 10 * 1024 * 1024, 
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
